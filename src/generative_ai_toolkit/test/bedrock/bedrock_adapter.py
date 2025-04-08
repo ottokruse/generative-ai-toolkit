7d6e063d5f3e785b650c0af032ac1a0f25e2cf72
kobler <kobler@amazon.com>
2025-04-08 08:37:00 +0200

    black format

--- src/generative_ai_toolkit/test/bedrock/bedrock_adapter.py
+++ src/generative_ai_toolkit/test/bedrock/bedrock_adapter.py
@@ -16,10 +16,7 @@ import boto3.session
 from datetime import datetime, timedelta
 import time
 import uuid
-from typing import (Any,
-                    Dict,
-                    Iterable,
-                    Protocol)
+from typing import Any, Dict, Iterable, Protocol
 from generative_ai_toolkit.tracer import Trace
 from generative_ai_toolkit.tracer.tracer import TraceScope
 
@@ -27,24 +24,26 @@ from generative_ai_toolkit.tracer.tracer import TraceScope
 class BedrockAgentAdapter(Protocol):
 
     def __init__(self, aget_id: str, aget_alias_id: str) -> None:
-        self.__bedrock_agent_runtime = boto3.client('bedrock-agent-runtime')
-        self.__conversation_id=uuid.uuid4().hex
-        self.__aget_id=aget_id
-        self.__aget_alias_id=aget_alias_id
-        self.__conversation_id=uuid.uuid4().hex
+        self.__bedrock_agent_runtime = boto3.client("bedrock-agent-runtime")
+        self.__conversation_id = uuid.uuid4().hex
+        self.__aget_id = aget_id
+        self.__aget_alias_id = aget_alias_id
+        self.__conversation_id = uuid.uuid4().hex
         self.__spans = []
 
-    def __invoke_agent(self,
-                       question: str,
-                       aget_id: str,
-                       aget_alias_id: str,
-                       conversation_id: str = uuid.uuid4().hex) -> tuple[list[str], list[str]]:
+    def __invoke_agent(
+        self,
+        question: str,
+        aget_id: str,
+        aget_alias_id: str,
+        conversation_id: str = uuid.uuid4().hex,
+    ) -> tuple[list[str], list[str]]:
         agent_response = self.__bedrock_agent_runtime.invoke_agent(
             agentId=aget_id,
             agentAliasId=aget_alias_id,
             sessionId=conversation_id,
             inputText=question,
-            enableTrace=True
+            enableTrace=True,
         )
 
         traces = []
@@ -58,14 +57,13 @@ class BedrockAgentAdapter(Protocol):
 
         return traces, chunks
 
-
     def converse(self, user_input: str) -> Any:
 
         bedrock_traces, chunks = self.__invoke_agent(
             question=user_input,
             aget_id=self.__aget_id,
             aget_alias_id=self.__aget_alias_id,  # Note: This was using conversation_id before
-            conversation_id=self.__conversation_id
+            conversation_id=self.__conversation_id,
         )
 
         model_inputs = []
@@ -86,18 +84,26 @@ class BedrockAgentAdapter(Protocol):
             orchestration_trace = brt.get("orchestrationTrace", {})
 
             if "modelInvocationInput" in orchestration_trace:
-                model_inputs.append(orchestration_trace.get("modelInvocationInput", None))
+                model_inputs.append(
+                    orchestration_trace.get("modelInvocationInput", None)
+                )
             if "modelInvocationOutput" in orchestration_trace:
-                model_outputs.append(orchestration_trace.get("modelInvocationOutput", None))
+                model_outputs.append(
+                    orchestration_trace.get("modelInvocationOutput", None)
+                )
             # if "rationale" in orchestration_trace:
             #     rationales.append(orchestration_trace.get("rationale", None))
             # if "invocationInput" in orchestration_trace :
             #     invocation_inputs.append(orchestration_trace.get("invocationInput", None))
             # if "observation" in orchestration_trace:
             #     observations.append(orchestration_trace.get("observation", None))
-            if orchestration_trace.get("invocationInput", {}).get("actionGroupInvocationInput", None):
+            if orchestration_trace.get("invocationInput", {}).get(
+                "actionGroupInvocationInput", None
+            ):
                 ag_input.append(orchestration_trace)
-            if orchestration_trace.get("observation", {}).get("actionGroupInvocationOutput", None):
+            if orchestration_trace.get("observation", {}).get(
+                "actionGroupInvocationOutput", None
+            ):
                 ag_output.append(orchestration_trace.get("observation", {}))
 
         first_bedrock_trace = bedrock_traces[0]
@@ -107,35 +113,40 @@ class BedrockAgentAdapter(Protocol):
         agent_version = first_bedrock_trace.get("agentVersion", None)
         session_id = first_bedrock_trace.get("sessionId", None)
 
-        final_response = (last_bedrock_trace.get("trace", {})
-                          .get("orchestrationTrace", {})
-                          .get("observation")
-                          .get("finalResponse")
-                          .get("text", None))
+        final_response = (
+            last_bedrock_trace.get("trace", {})
+            .get("orchestrationTrace", {})
+            .get("observation")
+            .get("finalResponse")
+            .get("text", None)
+        )
         trace_id = uuid.uuid4().hex
 
-        agent_span = Trace(trace_id=trace_id,
-                           # span_id=uuid.uuid4().hex[:16],
-                           span_name="agent-trace",
-                           span_kind="SERVER",
-                           started_at=datetime.now(),
-                           ended_at=datetime.now() + timedelta(microseconds=50),
-                           scope = TraceScope(name="agent-trace-scope",
-                                              version="1.0.0"),
-                           resource_attributes={
-                               "service.name": "agent-trace",
-                               "session.id": session_id
-                           },
-                           attributes={"user-input":  user_input,
-                                       "response": final_response,
-                                       "ai.conversation.id": self.__conversation_id,
-                                       "agent.id": agent_id,
-                                       "agent.alias.id": session_id,
-                                       "agent.version": agent_version,
-                                       "ai.trace.type": "converse",
-                                       "ai.agent.response":  final_response,
-                                       "duration_ms": 10})
-        self.__spans.append( agent_span)
+        agent_span = Trace(
+            trace_id=trace_id,
+            # span_id=uuid.uuid4().hex[:16],
+            span_name="agent-trace",
+            span_kind="SERVER",
+            started_at=datetime.now(),
+            ended_at=datetime.now() + timedelta(microseconds=50),
+            scope=TraceScope(name="agent-trace-scope", version="1.0.0"),
+            resource_attributes={
+                "service.name": "agent-trace",
+                "session.id": session_id,
+            },
+            attributes={
+                "user-input": user_input,
+                "response": final_response,
+                "ai.conversation.id": self.__conversation_id,
+                "agent.id": agent_id,
+                "agent.alias.id": session_id,
+                "agent.version": agent_version,
+                "ai.trace.type": "converse",
+                "ai.agent.response": final_response,
+                "duration_ms": 10,
+            },
+        )
+        self.__spans.append(agent_span)
 
         llm_invocation_spans = [
             self.__create_llm_invocation_span(
@@ -147,38 +158,41 @@ class BedrockAgentAdapter(Protocol):
                 agent_version=agent_version,
                 user_input=user_input,
                 final_response=final_response,
-                conversation_id=self.__conversation_id
+                conversation_id=self.__conversation_id,
             )
             for model_input, model_output in zip(model_inputs, model_outputs)
         ]
         self.__spans.extend(llm_invocation_spans)
 
-        tool_spans = [self.__create_tool_span(
-            tool_input=tool_input,
-            tool_output=tool_output,
-            trace_id=trace_id,
-            agent_span=agent_span,
-            session_id=session_id,
-            agent_id=agent_id,
-            agent_version=agent_version,
-            conversation_id=self.__conversation_id
-        ) for tool_input, tool_output in zip(ag_input, ag_output)]
+        tool_spans = [
+            self.__create_tool_span(
+                tool_input=tool_input,
+                tool_output=tool_output,
+                trace_id=trace_id,
+                agent_span=agent_span,
+                session_id=session_id,
+                agent_id=agent_id,
+                agent_version=agent_version,
+                conversation_id=self.__conversation_id,
+            )
+            for tool_input, tool_output in zip(ag_input, ag_output)
+        ]
         self.__spans.extend(tool_spans)
 
         return "".join(chunks)
 
     # TODO: make it static
     def __create_llm_invocation_span(
-            self,
-            model_input: Dict[str, Any],
-            model_output: Dict[str, Any],
-            parent_span: Trace,
-            agent_id: str,
-            session_id: str,
-            agent_version: str,
-            user_input: str,
-            final_response: str,
-            conversation_id: str
+        self,
+        model_input: Dict[str, Any],
+        model_output: Dict[str, Any],
+        parent_span: Trace,
+        agent_id: str,
+        session_id: str,
+        agent_version: str,
+        user_input: str,
+        final_response: str,
+        conversation_id: str,
     ) -> Trace:
 
         resource_attributes = {
@@ -186,7 +200,7 @@ class BedrockAgentAdapter(Protocol):
             "agent.id": agent_id,
             "agent.alias.id": session_id,
             "agent.version": agent_version,
-            "session.id": session_id
+            "session.id": session_id,
         }
 
         span_attributes = {
@@ -194,13 +208,15 @@ class BedrockAgentAdapter(Protocol):
             "ai.conversation.id": conversation_id,
             "ai.user.input": user_input,
             "ai.agent.response": final_response,
-            "ai.llm.request.inference.config": model_input.get("inferenceConfiguration", {}),
+            "ai.llm.request.inference.config": model_input.get(
+                "inferenceConfiguration", {}
+            ),
             "ai.llm.request.messages": model_input.get("text", {}),
             "ai.llm.request.model.id": "eu.anthropic.claude-3-sonnet-20240229-v1:0",
             "ai.llm.request.system": model_input.get("text", {}),
             "peer.service": "llm:claude-3-sonnet",
             "ai.llm.response.usage": model_output.get("metadata", {}).get("usage", {}),
-            "ai.llm.response.stop.reason": "tool_use"
+            "ai.llm.response.stop.reason": "tool_use",
         }
 
         start_time = datetime.now()
@@ -215,25 +231,29 @@ class BedrockAgentAdapter(Protocol):
             ended_at=start_time + timedelta(microseconds=50),
             scope=TraceScope(name="agent-tool-scope", version="1.0.0"),
             resource_attributes=resource_attributes,
-            attributes=span_attributes
+            attributes=span_attributes,
         )
 
     # TODO: make it static
-    def __create_tool_span(self,
-                           tool_input: Dict[str, Any],
-                           tool_output: Dict[str, Any],
-                           trace_id: str,
-                           agent_span: Trace,
-                           session_id: str,
-                           agent_id: str,
-                           agent_version: str,
-                           conversation_id: str) -> Trace:
+    def __create_tool_span(
+        self,
+        tool_input: Dict[str, Any],
+        tool_output: Dict[str, Any],
+        trace_id: str,
+        agent_span: Trace,
+        session_id: str,
+        agent_id: str,
+        agent_version: str,
+        conversation_id: str,
+    ) -> Trace:
         invocation_input = tool_input.get("invocationInput", {})
         action_group_input = invocation_input.get("actionGroupInvocationInput", {})
         tool_api_path = action_group_input.get("apiPath")
 
         if not tool_api_path:
-            raise ValueError("invocationInput.actionGroupInvocationInput.apiPath can not be None")
+            raise ValueError(
+                "invocationInput.actionGroupInvocationInput.apiPath can not be None"
+            )
 
         current_time = datetime.now()
 
@@ -242,7 +262,7 @@ class BedrockAgentAdapter(Protocol):
             "agent.id": agent_id,
             "agent.alias.id": session_id,
             "agent.version": agent_version,
-            "session.id": session_id
+            "session.id": session_id,
         }
 
         raw_response = tool_output.get("rawResponse", {})
@@ -253,7 +273,7 @@ class BedrockAgentAdapter(Protocol):
             "ai.tool.input": action_group_input,
             "ai.tool.name": tool_api_path,
             "ai.tool.output": raw_response.get("content"),
-            "peer.service": f"tool:{tool_api_path[1:]}"
+            "peer.service": f"tool:{tool_api_path[1:]}",
         }
 
         return Trace(
@@ -265,21 +285,21 @@ class BedrockAgentAdapter(Protocol):
             ended_at=current_time + timedelta(microseconds=50),
             scope=TraceScope(name="agent-tool-scope", version="1.0.0"),
             resource_attributes=resource_attrs,
-            attributes=span_attrs
+            attributes=span_attrs,
         )
 
     @property
     def traces(self) -> Iterable[Trace]:
         return self.__spans
 
-
     def reset(self) -> None:
-        self.__conversation_id=uuid.uuid4().hex
+        self.__conversation_id = uuid.uuid4().hex
         self.__spans.clear()
+
 
 if __name__ == "__main__":
     bedrockAgent_adapter = BedrockAgentAdapter()
-    answer = bedrockAgent_adapter.converse("What is the weather in Munich?" )
+    answer = bedrockAgent_adapter.converse("What is the weather in Munich?")
     traces = bedrockAgent_adapter.traces
 
     # tracer = OtlpTracer()
