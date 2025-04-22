5be36095508a55edad25afb64ad17218be31d0ed
Otto Kruse <ottokrus@amazon.nl>
2025-04-22 16:45:05 +0200

    chore: update copyright to 2025

--- tests/sample_agent_1.py
+++ tests/sample_agent_1.py
@@ -1,4 +1,4 @@
-# Copyright 2024 Amazon.com, Inc. and its affiliates. All Rights Reserved.
+# Copyright 2025 Amazon.com, Inc. and its affiliates. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
